9d72c2f3eb9af1773f1de77b14f62378e4c216fc
evan <evan@linux-stage>
2020-02-06 16:16:34 +0100

    New Version

--- test/Main.java
+++ test/Main.java
@@ -1,46 +1,55 @@
 package test;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    JFrame frame =new JFrame();
-    frame.setSize(1280, 900);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setLocationRelativeTo(null);
-    frame.setTitle("Ma première fenêtre");
-    
-    JPanel panel =new JPanel();
-    frame.setContentPane(panel);
-    panel.setBackground(Color.WHITE);
-    panel.add(new JButton("Click me"));
-    
-     
-    panel.add(getButton("3"));
-    panel.add(getButton("34"));
+		JFrame frame = new JFrame();
+		frame.setSize(1280, 900);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 
-    frame.setVisible(true);
-    
-    
-	}
-	
-	public static JButton getButton(String txt) {
-		JButton clickme = new JButton();
-	    clickme.setText(txt);
-	    clickme.setBounds(600, 600, 50, 70);
-	    clickme.setBackground(Color.LIGHT_GRAY);
-	    clickme.setFont(new Font("Arial",Font.ITALIC,20));
-	    clickme.setForeground(Color.RED);
-	    
-	    return clickme;
-	    
+		JPanel mainPanel = new JPanel();
+		frame.setContentPane(mainPanel);
+		mainPanel.setLayout(new BorderLayout(20, 20));
+		mainPanel.add(new JLabel("Formulaire Utilisateurs"), BorderLayout.NORTH);
+
+		JPanel panel = new JPanel();
+		mainPanel.add(panel, BorderLayout.CENTER);
+		panel.setBackground(Color.WHITE);
+		panel.setLayout(new GridLayout(4, 2, 2, 4));
+		JTextField firstname = new JTextField("Prénom");
+		panel.add(firstname);
+		JTextField lastname = new JTextField("Nom");
+		panel.add(lastname);
+		JTextField age = new JTextField("Âge");
+		panel.add(age);
+		JTextField mail = new JTextField("Émail");
+		panel.add(mail);
+		JTextField postalcode = new JTextField("Code Postal");
+		panel.add(postalcode);
+		JTextField city = new JTextField("Ville");
+		panel.add(city);
+		JCheckBox men = new JCheckBox("Homme");
+		panel.add(men);
+		JCheckBox women = new JCheckBox("Femme");
+		panel.add(women);
+
+		JButton jbutton = new JButton("Confirmer");
+		jbutton.setSize(0, 45);
+		mainPanel.add(jbutton, BorderLayout.SOUTH);
+
+		frame.setVisible(true);
 	}
 
 }
